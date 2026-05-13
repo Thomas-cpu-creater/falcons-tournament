a8f29bd73aae60e01f4b300fb9aae85a63048731
Thomas-cpu-creater <tjonssonmail@gmail.com>
2026-05-13 07:39:51 +0200

    Update App.jsx

--- falcons-tournament/src/App.jsx
+++ falcons-tournament/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import { db } from "./firebase.js";
 import { ref, set, onValue } from "firebase/database";
 
